e5ad01b88279dfe0822889929975cb3edefe1815
syarifhidayatullah <syarif.h@go-jek.com>
2019-11-22 15:45:00 +0700

    Refactoring finance_use case beam

--- finance_use_case/beam/main.py
+++ finance_use_case/beam/main.py
@@ -1,59 +1,9 @@
-import logging
-
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 
-
-class LeftJoin(beam.PTransform):
-    """This PTransform performs a left join given source_pipeline_name, source_data,
-     join_pipeline_name, join_data, common_key constructors"""
-
-    def __init__(self, source_pipeline_name, source_data, join_pipeline_name, join_data, common_key):
-        self.join_pipeline_name = join_pipeline_name
-        self.source_data = source_data
-        self.source_pipeline_name = source_pipeline_name
-        self.join_data = join_data
-        self.common_key = common_key
-
-    def expand(self, pcolls):
-        def _format_as_common_key_tuple(data_dict, common_key):
-            return data_dict[common_key], data_dict
-
-        """This part here below starts with a python dictionary comprehension in case you 
-        get lost in what is happening :-)"""
-        return ({pipeline_name: pcoll | 'Convert to ({0}, object) for {1}'
-                .format(self.common_key, pipeline_name)
-                                        >> beam.Map(_format_as_common_key_tuple, self.common_key)
-                 for (pipeline_name, pcoll) in pcolls.items()}
-                | 'CoGroupByKey {0}'.format(pcolls.keys()) >> beam.CoGroupByKey()
-                | 'Unnest Cogrouped' >> beam.ParDo(UnnestCoGrouped(),
-                                                   self.source_pipeline_name,
-                                                   self.join_pipeline_name)
-                )
-
-
-class UnnestCoGrouped(beam.DoFn):
-    """This DoFn class unnests the CogroupBykey output and emits """
-
-    def process(self, input_element, source_pipeline_name, join_pipeline_name):
-        group_key, grouped_dict = input_element
-        join_dictionary = grouped_dict[join_pipeline_name]
-        source_dictionaries = grouped_dict[source_pipeline_name]
-        for source_dictionary in source_dictionaries:
-            try:
-                source_dictionary.update(join_dictionary[0])
-                yield source_dictionary
-            except IndexError:  # found no join_dictionary
-                yield source_dictionary
-
-
-class LogContents(beam.DoFn):
-    """This DoFn class logs the content of that which it receives """
-
-    def process(self, input_element):
-        logging.info("Contents: {}".format(input_element))
-        logging.info("Contents type: {}".format(type(input_element)))
-        return
+from join import LeftJoin
+from log_contents import LogContents
+from unnest_co_grouped import UnnestCoGrouped
 
 
 def main():
---
+++ finance_use_case/beam/unnest_co_grouped.py
@@ -0,0 +1,16 @@
+import apache_beam as beam
+
+
+class UnnestCoGrouped(beam.DoFn):
+    """This DoFn class unnests the CogroupBykey output and emits """
+
+    def process(self, input_element, source_pipeline_name, join_pipeline_name):
+        group_key, grouped_dict = input_element
+        join_dictionary = grouped_dict[join_pipeline_name]
+        source_dictionaries = grouped_dict[source_pipeline_name]
+        for source_dictionary in source_dictionaries:
+            try:
+                source_dictionary.update(join_dictionary[0])
+                yield source_dictionary
+            except IndexError:  # found no join_dictionary
+                yield source_dictionary
